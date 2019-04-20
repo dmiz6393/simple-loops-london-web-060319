8d9dbb068f584c5c3a36aaeace6e62fa9fb1fe81
Danielle Mizrachi <dmiz@brandeis.edu>
2019-04-20 20:13:32 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,8 +2,9 @@ def loop_iterator(number_of_times)
   counter=0
   phrase="Welcome to Flatiron School's Web Development Course!"
   loop do 
+        counter +=1
     puts phrase
-    counter +=1
+
     if counter == 7
       break
 end 
