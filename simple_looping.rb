640500bcee7592f3d22dfc2a8c1b0e341d423f9f
Danielle Mizrachi <dmiz@brandeis.edu>
2019-04-20 20:17:37 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -38,7 +38,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for number in 1..number_of_times
+  for number in 1..number_of_times do
   puts phrase
 end
 end
